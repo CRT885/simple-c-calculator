b821d1250d1aa15a9be7e5ec538bc2fbd7585d06
CRT885 <christinrajesh56@gmail.com>
2026-06-15 12:20:10 +0530

    Add files via upload

---
+++ cal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+	//Declaration of variables for simple calculator
+	int a=0,b=0,y=0;
+	char operator='0';
+	char add='+',sub='-',mul='*',div='/';
+	//Enter the value of variables
+	printf("Enter the value of A:");
+	scanf("%d",&a);
+	printf("Enter the value of B:");
+	scanf("%d",&b);
+	printf("Enter the Operator:");
+	scanf("%c",&operator);
+	//Arithematic operations using if statements!
+	if(operator==add)
+	y=a+b;
+	
+	if(operator==sub)
+	y=a-b;
+	
+	if(operator==mul)
+	y=a*b;
+	
+	if(operator==div)
+	y=a/b;
+    //Output result
+    printf("The value of %d%c%d is %d",a,operator,b,y);
+    return 0;
+}
